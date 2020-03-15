9b4703f1d549fab30160fbda46396a2cbb2b2fb1
Wende Lu <wendelu1991@gmail.com>
2020-03-15 23:32:19 +0800

    primitive_types

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,13 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` for hints!!
 
-// I AM NOT DONE
+use std::convert::TryInto;
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice:[i32;3] = a[1..4].try_into().unwrap();;
 
     assert_eq!([2, 3, 4], nice_slice)
 }
